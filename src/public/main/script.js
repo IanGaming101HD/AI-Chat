b580c372be57e1e5e8176847488429ebbb58d3a0
IanGaming101HD <100050322+IanGaming101HD@users.noreply.github.com>
2023-07-27 11:50:22 +0100

    a

--- src/public/main/script.js
+++ src/public/main/script.js
@@ -1,31 +1,75 @@
-const chat = require('../../schemas/chatSchema');
-const search = document.getElementById('search')
-const searchButton = document.getElementById('search-button')
+// const mongoose = require('mongoose');
+// const chat = require('../../schemas/chatSchema');
+const questionInput = document.getElementById('question-input')
+const submitButton = document.getElementById('submit-button')
 const noDataContainer = document.getElementById('no-data-container')
 
-// let data = true
-let data = chat.find()
-    .then((users) => {
-        console.log('All users:', users);
-    })
-    .catch((error) => {
-        console.error('Error finding users:', error);
-    });
-
-if (!data) {
-    // noDataContainer.style.display = 'block'
-    // noDataContainer.style.display = 'inline'
-} else {
-    noDataContainer.style.display = 'none'
+function hideEntireDiv(div) {
+    div.hidden = true
+    Array.from(div.children).forEach((child) => child.hidden = true)
 }
 
-searchButton.addEventListener('click', (event) => {
-    event.preventDefault()
-    if (!search.value) return;
+function showEntireDiv(div) {
+    div.hidden = false
+    Array.from(div.children).forEach((child) => child.hidden = false)
+}
 
-    let newMessage = new chat({
+let chatContent = localStorage.getItem('chat-content')
+
+if (chatContent) {
+    hideEntireDiv(noDataContainer)
+    chatContent = JSON.parse(localStorage.getItem('chat-content'))
+} else {
+    chatContent = []
+}
+
+// // let data = true
+// let data = chat.find()
+//     .then((users) => {
+//         console.log('All users:', users);
+//     })
+//     .catch((error) => {
+//         console.error('Error finding users:', error);
+//     });
+
+// if (!data) {
+//     // noDataContainer.style.display = 'block'
+//     // noDataContainer.style.display = 'inline'
+// } else {
+//     noDataContainer.style.display = 'none'
+// }
+
+let user = true
+
+submitButton.addEventListener('click', (event) => {
+    event.preventDefault()
+    if (!questionInput.value) return;
+
+    if (noDataContainer.hidden) {
+        hideEntireDiv(noDataContainer)
+    }
+
+    chatContent.push({
         user: true,
-        message: search.value
-    });
-    newMessage.save()
+        message: questionInput.value
+    })
+    localStorage.setItem('chat-content', JSON.stringify(chatContent))
+
+    
+    // <div class='user-message message'>Hi</div>
+    // <div class='bot-message message'>Hello</div>
+
+    let dataContainer = document.getElementById('data-container')
+    let newMessage = document.createElement('div')
+    dataContainer.appendChild(newMessage)
+    newMessage.classList.add('message')
+    user ? newMessage.classList.add('user-message') : newMessage.classList.add('bot-message')
+    user = !user
+    newMessage.innerText = questionInput.value
+
+    // let newMessage = new chat({
+    //     user: true,
+    //     message: search.value
+    // });
+    // newMessage.save()
 })
